5ddb951c312d9c4faa18c9f5be1dddc77ce8dc59
Juwon Park <rooopy96@gmail.com>
2021-11-30 18:23:02 +0900

    JS: Add click event (navbar)

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const menuLogoBtn = document.querySelector(".navbar__menu__logo");
+
+menuLogoBtn.addEventListener("click", () => {
+	const navbarMenu = document.querySelector(".navbar__menu");
+	const home = document.querySelector(".home");
+	
+	navbarMenu.classList.toggle("navbar__menu-active");
+	home.classList.toggle("menu-active");
+})
